566bf093cbecc2d0148c4771cde0222a89034df3
Jared Ackley <jrod3323@gmail.com>
2021-03-08 21:48:09 -0600

    remove unnecessary

--- src/App.js
+++ src/App.js
@@ -1,10 +1,6 @@
 import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import NavTabs from "./components/NavTabs";
-import About from "./components/pages/About"; 
-import Discover from "./components/pages/Discover";
-import Search from "./components/pages/Search"; 
-
 
 function App() {
   return (
